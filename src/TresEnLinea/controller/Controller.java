86c4ca78ddd87b3714dec96b0adbb6ef87ce7c18
JRodriguezZ <jrodriguezz00@elpuig.xeill.net>
2021-02-23 17:54:22 +0100

    4

--- src/TresEnLinea/controller/Controller.java
+++ src/TresEnLinea/controller/Controller.java
@@ -11,51 +11,85 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    private ToggleButton casella00, casella10, casella20, casella01, casella11, casella21, casella02, casella12, casella22;
+    @FXML private ToggleButton casella00, casella10, casella20, casella01, casella11, casella21, casella02, casella12, casella22;
 
-    @FXML
-    private TextField nomJugador1, nomJugador2;
+    @FXML private TextField nomJugador1, nomJugador2;
 
-    @FXML
-    private MenuItem menuItemReinciar, menuItemTancar, menuItemFosc, menuItemClar, menuItemClassificacio, menuItemGuia, menuItemSobre;
+    @FXML private MenuItem menuItemReinciar, menuItemTancar, menuItemFosc, menuItemClar, menuItemClassificacio, menuItemGuia, menuItemSobre;
 
-    @FXML
-    private Label tornDisplay;
+    @FXML private Label tornDisplay;
 
-    @FXML
-    private Button botoNovaPartida;
+    @FXML private Button botoNovaPartida;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        nomJugador1.setText("Jugador 1");
+        nomJugador2.setText("Jugador 2");
+
+        decidirTorn();
+
+        netejarCaselles();
     }
 
     int torn = 0;
-    int defaultval = 9;
     int[][] casella = new int[3][3];
 
     public void tirada(ActionEvent actionEvent) {
         ToggleButton b = (ToggleButton) actionEvent.getSource();
 
+        int i = Integer.parseInt(b.getId().substring(7,8));
+        int x = Integer.parseInt(b.getId().substring(8,9));
+
         if (torn %2 == 0){
-            b.setText("O");
-            int i = Integer.parseInt(b.getId().substring(7,8));
-            int x = Integer.parseInt(b.getId().substring(8,9));
-            casella[i][x] = 1;
+            if (casella[i][x] == 1 || casella[i][x] == 2) {
+                b.setSelected(true);
+            } else {
+                b.setText("O");
+                casella[i][x] = 1;
+            }
             tornDisplay.setText("Torn del jugador 1");
+
         } else {
-            b.setText("X");
-            int i = Integer.parseInt(b.getId().substring(7,8));
-            int x = Integer.parseInt(b.getId().substring(8,9));
-            casella[i][x] = 2;
+            if (casella[i][x] == 1 || casella[i][x] == 2) {
+                b.setSelected(true);
+            } else {
+                b.setText("X");
+                casella[i][x] = 2;
+            }
             tornDisplay.setText("Torn del jugador 2");
+
         }
 
         winCondition();
         torn++;
     }
+
+
+    private void tiradaCPU(ActionEvent actionEvent) {
+    }
+
+    public void startNovaPartida(ActionEvent actionEvent) {
+
+        Jugador j1 = new Jugador(nomJugador1.toString());
+
+        Jugador j2 = new Jugador(nomJugador2.toString());
+
+        nomJugador1.setEditable(false);
+        nomJugador2.setEditable(false);
+
+        if (j1.equals("") || j2.equals("")) {
+//            tiradaCPU();
+        }
+
+        torn = 0;
+
+        decidirTorn();
+
+        netejarCaselles();
+    }
+
 
     public void winCondition() {
         boolean winJ1 = false;
@@ -69,8 +103,7 @@ public class Controller implements Initializable {
                 casella[0][0] + casella[1][1] + casella[2][2] == 3 ||
                 casella[0][2] + casella[1][1] + casella[2][0] == 3 ) {
             winJ1 = true;
-        }
-        if (casella[0][0] + casella[0][1] + casella[0][2] == 6 ||
+        } else if (casella[0][0] + casella[0][1] + casella[0][2] == 6 ||
                 casella[1][0] + casella[1][1] + casella[1][2] == 6 ||
                 casella[2][0] + casella[2][1] + casella[2][2] == 6 ||
                 casella[0][0] + casella[1][0] + casella[2][0] == 6 ||
@@ -84,30 +117,30 @@ public class Controller implements Initializable {
 
         if (winJ1){
             tornDisplay.setText(nomJugador1.getText() + " ha guanyat!");
+            nomJugador1.setEditable(true);
+            nomJugador2.setEditable(true);
         } else if (winJ2) {
             tornDisplay.setText(nomJugador2.getText() + " ha guanyat!");
+            nomJugador1.setEditable(true);
+            nomJugador2.setEditable(true);
         }
 
     }
 
-    public void startNovaPartida(ActionEvent actionEvent) {
-
-        Jugador j1 = new Jugador(nomJugador1.toString());
-
-        Jugador j2 = new Jugador(nomJugador2.toString());
-
+    public void decidirTorn() {
         int primer = (int) (Math.random() * 2 + 1);
 
         if (primer == 1) tornDisplay.setText("Torn del jugador 1");
         else if (primer == 2) tornDisplay.setText("Torn del jugador 2");
+    }
+
+    public void netejarCaselles() {
 
         for (int i = 0; i < casella.length; i++) {
             for (int j = 0; j < casella.length; j++) {
                 casella[i][j] = 9;
             }
         }
-
-        torn = 0;
 
         casella00.setText("");
         casella00.setSelected(false);
